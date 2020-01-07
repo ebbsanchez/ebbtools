671b3cd0118da8aa1764a30cea6053b96f3dffe3
Jack Sanchez <evo@gms.tku.edu.tw>
2020-01-07 18:05:01 +0800

    add allowed host

--- practicezone/practicezone/production_settings.py
+++ practicezone/practicezone/production_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u%^-&#djze$a7)ykn!=ub*7n)!#sfu!oifq#nzgir&)nj-2qu='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["afternoon-beach-38581.herokuapp.com"]
 
 
 # Application definition
