113d468022d51d641c591a9d8a344b4f3308f8be
Jack Sanchez <evo@gms.tku.edu.tw>
2020-01-07 20:09:45 +0800

    change from sqlite3 to postgresql.

--- practicezone/practicezone/settings.py
+++ practicezone/practicezone/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'practicezone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'practicezone',
+        'USER': 'jacklin',
+        'PASSWORD': 'ssss',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
