4646d0c28ca0c18f4af8864af65ac043ab7d5280
Jack Sanchez <evo@gms.tku.edu.tw>
2020-01-07 20:11:54 +0800

    bug fix

--- practicezone/practicezone/production_settings.py
+++ practicezone/practicezone/production_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u%^-&#djze$a7)ykn!=ub*7n)!#sfu!oifq#nzgir&)nj-2qu='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["afternoon-beach-38581.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'practicezone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'practicezone',
+        'USER': 'jacklin',
+        'PASSWORD': 'ssss',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -119,4 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
