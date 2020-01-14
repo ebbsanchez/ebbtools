905bf8cf72efaa42bd527d79553c1ffc2397ae2b
Jack Sanchez <evo@gms.tku.edu.tw>
2020-01-14 23:26:44 +0800

    Fix BUG

--- practicezone/practicezone/production_settings.py
+++ practicezone/practicezone/production_settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u%^-&#djze$a7)ykn!=ub*7n)!#sfu!oifq#nzgir&)nj-2qu='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["afternoon-beach-38581.herokuapp.com"]
 
--- practicezone/practicezone/wsgi.py
+++ practicezone/practicezone/wsgi.py
@@ -11,6 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicezone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicezone.production_settings')
 
 application = Cling(get_wsgi_application())
