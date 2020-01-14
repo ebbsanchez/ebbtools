095b98a93ce07183126c8b3d76790568c6ffe348
Jack Sanchez <evo@gms.tku.edu.tw>
2020-01-14 22:27:07 +0800

    Update Production Settings

--- practicezone/practicezone/production_settings.py
+++ practicezone/practicezone/production_settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todolist.apps.TodolistConfig',
+    'accounts',
+    'products',
+    'shopping_cart',
 ]
 
 MIDDLEWARE = [
