110be693352883ca911102f0db73a6abd77f3ce1
Jack Sanchez <evo@gms.tku.edu.tw>
2020-02-05 17:47:56 +0800

    Add gitlog app into index.

--- practicezone/practicezone/views.py
+++ practicezone/practicezone/views.py
@@ -2,17 +2,32 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .test_link_validator import LinkTest
 from diary.models import Entry
+from gitlog.models import Commit
 
 # Create your views here.
 
 
 def index(request):
     entries = Entry.objects.order_by('-posted_date')
+    commits = Commit.objects.all()
+    formated_commits = []
+    for commit in commits:
+        formated_commit = {}
+        formated_commit['weekday'] = commit.datetime_object.strftime('%a')
+        formated_commit['day'] = commit.datetime_object.strftime('%d')
+        formated_commit['branch'] = commit.branch
+        formated_commit['insertions_count'] = commit.insertions_count
+        formated_commit['deletions_count'] = commit.deletions_count
+        formated_commit['commit_note'] = commit.commit_note
+        formated_commits.append(formated_commit)
+
     context = {
         'entries': entries,
+        'commits': formated_commits,
+
     }
 
-    return render(request, "index.html", context)
+    return render(request, "index_gitlog.html", context)
 
 
 def showTheTestResult(request):
